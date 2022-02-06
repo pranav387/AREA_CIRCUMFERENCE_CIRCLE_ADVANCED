d5eef61b0d7635a86acdef7d2dfc48105464c0f6
pranav387 <71219026+pranav387@users.noreply.github.com>
2022-02-06 16:57:17 +0530

    Add files via upload

---
+++ programSETTINGS.py
@@ -0,0 +1,5 @@
+RUN_PROGRAM = True
+ROUND_OFF_VALUES = True
+SHOW_ONLY_AREA = False
+DEFAULT_PATH = "C:\\Users\\prana\\Desktop\\vscode_venv_projects\\portfolio_programs\\area_of_circle"
+ALWAYS_SET_TO_DEFAULT_PATH = True
